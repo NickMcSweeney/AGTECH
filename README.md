548287fa8222b57c2254dfcd70d2e8d2035e8f40
Nicholas Shindler <shindler25@gmail.com>
2020-07-02 15:02:14 +0200

    configure the laser sensor to output pointcloud

--- README.md
+++ README.md
@@ -27,3 +27,20 @@ this is for harvest vehicle robots, property of a private company and therefore 
 	|--- *sensors*
 	|--- *services*
 ```
+
+#### Notes: 
+I'm a little confused about the context of these questions. From the point of view of MindProbe the issue wasn't getting all of the Sick data, it was displaying the data in a useful way. 
+We either displayed a big wedge in front of the robot bounded by the Sick samples or a color coded ray for each sample. The latter was mostly only useful in still frames.
+
+The format of the data should be well documented by Sick. In general, the scan data was in polar coordinates from the center of the Lidar. We got useful information as follows:
+- All transformations are assumed to be 2D with the plane of the Lidar sweep parallel to the ground.
+- The samples are transformed from polar to cartesian coordinates relative to the Lidar device.
+- The time of the start of the scan is either inferred from the arrival time of the data or in the data itself.
+- The rate of sweep is well known so the time that each sample was taken can be inferred.
+- Wheel odometry is used to localize the robot at the time each sample was taken.
+- The samples are finally transformed into "absolute" (field) coordinates given the robot's pose and the Lidar's fixed position relative to the robot's origin.
+
+Scan modes should be well documented by Sick. We only used the one described above.
+
+Data rate was never a big issue. The ethernet connection from the Lidar was adequate. We were also able to transmit the transformed Lidar sample data, including the return intensity to our MindProbe telemetry application over wifi in real time.
+
--- hv_bridge/includes/Sensors/lidar.py
+++ hv_bridge/includes/Sensors/lidar.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from std_msgs.msg import Float64
 from std_msgs.msg import Int16MultiArray
+from nav_msgs.msg import PointCloud2
+from nav_msgs.msg import PointField
 import time
 import ros
 import sys, os
@@ -14,30 +16,54 @@ class LidarCamera(Getter):
     # Lidar sensor class extending the getter, for handling the publishing of data 
     ###
     def init(self, ros, hostname): 
+        self.hostname = hostname
         # create publisher
         self.pub = ros.Publisher('/'+hostname+'/lidar/'+resource_name(self.name),Int16MultiArray,queue_size=1)
         # pass to parent to enble probes
         #self.mp.write_probe(self.codes,1)
         Getter.init(self)
 
-    def run(self, q):
+    def run(self, q, current_time):
         ## handles the publishing of data, should be run at an acceptable update interval
         # get data from queue
 
 
         data = q[self.name]
         
+        # get the data from the message
         data = data.split('v=')[-1]
         if (data == ''):
             return
+
+        # create x and y arrays from the base16 string of data
         strarr = data.split(':')
-        intarr = []
-        for elem in strarr:
-            if (elem != ''):
-                intarr.append(int(elem,16))
+        x=[]
+        y=[]
+        (lambda a : (x.append(int(a[0],16)),y.append(int(a[1],16)), fx(a[2:])) if a.size > 1  else False)(strarr)
         
-        dataout = Int16MultiArray()
-        dataout.data = intarr
+        # create pointcloud2 message
+        dataout = PointCloud2()
+        # add timestamp based on ros time
+        msg.header.stamp = current_time
+        # add frame 
+        msg.header.frame_id = self.hostname
+        # format message
+        N = len(x)
+        xy = np.array(np.hstack([x,y]), dtype=np.float32)
+        msg.height = 1
+        msg.width = N
+
+        msg.fields = [
+            PointField('x', 0, PointField.FLOAT32, 1),
+            PointField('y', 4, PointField.FLOAT32, 1),
+        ]
+        msg.is_bigendian = False
+        msg.point_step = 8
+        msg.row_step = msg.point_step * N
+        msg.is_dense = True;
+        msg.data = xy.tostring()
+
+
         # publish data
         Getter.run(self, dataout)
 
